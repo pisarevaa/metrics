b640e2cec8dbe2ac56993e8f430bf6ac2ff809b0
Andrei Pisarev <pisarev_aa@mail.ru>
2024-03-31 09:38:38 +0300

    Обновление структуры хранения и логики

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,66 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 type MemStorage struct {
-	metrics []Metric
+	metrics MetricGroup
 }
 
-type Metric struct {
-	metricName  string
-	metricType  string
-	metricValue string
+type MetricGroup struct {
+	gauge  map[string]float64
+	counter  map[string]int64
 }
 
-// Handle metrics logs
-func (st *MemStorage) handleMetrics(res http.ResponseWriter, req *http.Request) {
+
+func (ms *MemStorage) handleMetrics(res http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
 		return
 	}
-	// Get path params
+
 	pathParams := strings.Split(strings.TrimPrefix(req.URL.Path, "/update/"), "/")
 	if len(pathParams) != 3 {
 		http.Error(res, "Path should contains all three fields!", http.StatusNotFound)
 		return
 	}
 
-	metric := Metric{
-		metricName:  pathParams[1],
-		metricType:  pathParams[0],
-		metricValue: pathParams[2],
-	}
+	metricType, metricName, metricValue := pathParams[0], pathParams[1], pathParams[2]
 
-	if !(metric.metricType == "gauge" || metric.metricType == "counter") {
+	if !(metricType == "gauge" || metricType == "counter") {
 		http.Error(res, "Only 'gauge' and 'counter' values are not allowed!", http.StatusBadRequest)
 		return
 	}
-	if metric.metricName == "" {
+	if metricName == "" {
 		http.Error(res, "Empty metricName is not allowed!", http.StatusNotFound)
 		return
 	}
-	if metric.metricValue == "" || metric.metricValue == "none" {
+	if metricValue == "" || metricValue == "none" {
 		http.Error(res, "Empty metricValue is not allowed!", http.StatusBadRequest)
 		return
 	}
 
-	st.AddItem(metric)
+	if metricType == "gauge" {
+		floatValue, err := strconv.ParseFloat(metricValue, 64)
+		if err != nil {
+			http.Error(res, "metricValue is not corect float!", http.StatusBadRequest)
+			return
+		}
+		ms.metrics.gauge[metricName] = floatValue
+	}
 
-	res.WriteHeader(http.StatusOK)
-}
-
-
-func (st *MemStorage) AddItem(metric Metric) []Metric {
-    st.metrics = append(st.metrics, metric)
-    return st.metrics
+	if metricType == "counter" {
+		intValue, err := strconv.ParseInt(metricValue, 10, 64)
+		if err != nil {
+			http.Error(res, "metricValue is not correct integer!", http.StatusBadRequest)
+			return
+		}
+		ms.metrics.counter[metricName] += intValue
+	}
+	fmt.Println("ms.metrics.gauge ", ms.metrics.gauge)
+	fmt.Println("ms.metrics.counter ", ms.metrics.counter)
+	// res.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	metrics := make([]Metric, 0)
-	storage := MemStorage{metrics}
+	gauge := make(map[string]float64)
+	counter := make(map[string]int64)
+	storage := MemStorage{metrics: MetricGroup{gauge, counter}}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", storage.handleMetrics)
 	err := http.ListenAndServe(":8080", mux)
