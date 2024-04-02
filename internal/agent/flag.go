bfd7183551c817bdd374d7bd2abe572b55ad55b5
Andrei Pisarev <pisarev_aa@mail.ru>
2024-04-02 21:33:23 +0300

    Поправил конфиги

--- internal/agent/flag.go
+++ internal/agent/flag.go
@@ -14,8 +14,8 @@ var settings Settings
 
 func Init() Settings {
 	flag.StringVar(&settings.serverHost, "a", "localhost:8080", "server host")
-	flag.IntVar(&settings.pollIntervalSec, "r", 2, "frequency of sending metrics to the server")
-	flag.IntVar(&settings.reportInterval, "w", 10, "frequency of polling metrics from the runtime package")
+	flag.IntVar(&settings.pollIntervalSec, "p", 2, "frequency of sending metrics to the server")
+	flag.IntVar(&settings.reportInterval, "r", 10, "frequency of polling metrics from the runtime package")
 	flag.Parse()
 	if len(flag.Args()) > 0 {
 		panic("used not declared arguments")
