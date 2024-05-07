bd91d8237373467e6bb50095c4e3f7eb4e8a9e57
Andrei Pisarev <pisarev_aa@mail.ru>
2024-05-07 20:04:09 +0300

    Убрал Println

--- internal/server/database.go
+++ internal/server/database.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
@@ -12,7 +11,6 @@ func ConnectDB(config Config, logger *zap.SugaredLogger) *pgxpool.Pool {
 	if config.DatabaseDSN == "" {
 		return nil
 	}
-	fmt.Println("config.DatabaseDSN", config.DatabaseDSN)
 	dbpool, err := pgxpool.New(context.Background(), config.DatabaseDSN)
 	if err != nil {
 		logger.Error("Unable to create connection pool: %v", err)
