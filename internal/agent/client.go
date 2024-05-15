f98e6af1bc4ac298f3b29a3503cf0f2692566443
Andrei Pisarev <pisarev_aa@mail.ru>
2024-05-15 21:07:28 +0300

    Добавил omitempty

--- internal/agent/client.go
+++ internal/agent/client.go
@@ -1,10 +1,29 @@
 package agent
 
 import (
+	"strings"
+	"time"
+
 	"github.com/go-resty/resty/v2"
 )
 
+const retries = 3
+const retryWaitTime = 1
+const retryMaxWaitTime = 20
+
 func NewClient() *resty.Client {
 	client := resty.New()
+	client.
+		SetRetryCount(retries).
+		SetRetryWaitTime(retryWaitTime * time.Second).
+		SetRetryMaxWaitTime(retryMaxWaitTime * time.Second).
+		AddRetryCondition(
+			func(_ *resty.Response, err error) bool {
+				if err == nil {
+					return false
+				}
+				return strings.Contains(err.Error(), "connect: connection refused")
+			},
+		)
 	return client
 }
